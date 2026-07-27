b7c220255452094cb38a6e057964dfc4b24b53af
Ryan <ryanshi909@gmail.com>
2026-07-27 13:44:49 -0400

    Fix somethings

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
-#include <bits/stdc++.h>
 #include "C:\Users\ryans\Desktop\Code\BlackholeSim\Physics\CelestialBodies.h"
+#include "Math/Vector3.h"
 
 int main(){
-    CelestialBodies Earth(Earth,);
+    CelestialBodies Earth("Earth", 5.9722e24, Vector3(1.496e11, 0, 0),Vector3(0 ,29.78 ,0));
+    CelestialBodies Sun("Sun", 1.972e24, Vector3(0, 0, 0), Vector3(0, 0, 0));
     return 0;
 }
