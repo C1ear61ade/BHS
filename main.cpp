0cb61e17464d260e3a9dbecdcc0f654037d992cf
Ryan <ryanshi909@gmail.com>
2026-07-29 17:51:19 -0400

    Deleted some things to restart

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "Math/Vector3.h"
 #include <bits/stdc++.h>
 
+//----------------------------------------------------------------------------------------------------------------
+
 struct Position {
     double bodyX;
     double bodyY;
@@ -12,6 +14,8 @@ Position CelestialBodyPosition(const Vector3& position, int windowWidth, int win
     constexpr double scale = 1e9;
     return {windowWidth / 2.0 + position.x / scale, windowLength / 2.0 + position.y / scale};
 }
+
+//----------------------------------------------------------------------------------------------------------------
 
 void DrawBody(double x, double y, double radius) {
     glBegin(GL_TRIANGLE_FAN);
@@ -29,10 +33,12 @@ void DrawBody(double x, double y, double radius) {
     glEnd();
 }
 
+//----------------------------------------------------------------------------------------------------------------
+
 int main(){
 
-    CelestialBodies Earth("Earth",5.9722e24,6371000.0,Vector3(1.496e11, 0, 0),Vector3(0 ,29.78 ,0));
-    CelestialBodies Sun("Sun", 1.972e24, 696340000.0, Vector3(0, 0, 0), Vector3(0, 0, 0));
+    CelestialBodies Earth("Earth",5.9722e24,6371000.0,Vector3(1.496e11, 0, 0),Vector3(0 ,29780 ,0));
+    CelestialBodies Sun("Sun", 1.989e30, 696340000.0, Vector3(0, 0, 0), Vector3(0, 0, 0));
 
     if (!glfwInit()) {
         std::cout << "GLFW failed because of an unknown error. Please check on your own accord." << std::endl;
@@ -50,24 +56,7 @@ int main(){
 
     while (!glfwWindowShouldClose(window)) {
 
-        int width, height;
-        constexpr double scale = 1e9;
-
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-
-        glfwGetFramebufferSize(window, &width, &height);
-        glOrtho(0, width, height, 0, -1, 1);
-
-        double Xcenter = width / 2.0;
-        double Ycenter = height / 2.0;
-
-        Position sunPosition = CelestialBodyPosition(Sun.position, width, height);
-        Position earthPosition = CelestialBodyPosition(Earth.position, width, height);
-
     }
-
     glfwTerminate();
     return 0;
 }
